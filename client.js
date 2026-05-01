0e1b6f99a73fdab2a4f1aed970cd4b1e3facd610
Raizz <bwab20465@gmail.com>
2026-05-01 09:34:53 +0700

    Create user.js

---
+++ client.js
@@ -0,0 +1,72 @@
+const io = require("socket.io-client");
+const readline = require("readline-sync");
+const chalk = require("chalk");
+
+console.clear();
+
+// TITLE
+console.log(chalk.cyan("╔══════════════════════╗"));
+console.log(chalk.cyan("║    RANZ X SPY        ║"));
+console.log(chalk.cyan("╚══════════════════════╝\n"));
+
+// PASSWORD
+let pass = readline.question("Masukkan sandi: ", { hideEchoBack: true });
+
+if (pass !== "1234") {
+    console.log(chalk.red("❌ Sandi salah"));
+    process.exit();
+}
+
+// INPUT NAMA
+let name = readline.question("Masukkan nama kamu: ");
+
+// GANTI IP JIKA BEDA DEVICE
+const socket = io("http://localhost:3000");
+
+socket.on("connect", () => {
+    socket.emit("register", name);
+    console.log(chalk.green("\n✔ Terhubung ke server sebagai: " + name));
+
+    startScan();
+});
+
+// SCAN
+function startScan() {
+    let progress = 0;
+
+    const interval = setInterval(() => {
+        process.stdout.write("\x1Bc");
+
+        console.log("╔══════════════════════╗");
+        console.log(`║ ANALYZING... ${progress}%      ║`);
+        console.log("╚══════════════════════╝");
+
+        progress++;
+
+        if (progress > 100) {
+            clearInterval(interval);
+
+            console.log("\n✔ Analisis selesai!\n");
+
+            console.log("📌 Deskripsi:");
+            console.log("Cara menggunakan: generate link lalu bagikan secara bijak untuk tujuan demonstrasi atau edukasi.\n");
+
+            console.log("⚠ PERINGATAN:");
+            console.log("Gunakan hanya untuk pembelajaran. Jangan digunakan untuk melanggar privasi atau merugikan orang lain.");
+            console.log("Script by Raizz\n");
+
+            console.log("🔗 Akses website:");
+            console.log("👉 https://webcam360.org/\n");
+        }
+    }, 50);
+}
+
+// JIKA DIBLOK
+socket.on("blocked", () => {
+    console.clear();
+    console.log(chalk.red("╔══════════════════════╗"));
+    console.log(chalk.red("║     AKSES DITOLAK    ║"));
+    console.log(chalk.red("╚══════════════════════╝\n"));
+    console.log(chalk.red("⛔ Kamu telah diblokir oleh admin!"));
+    process.exit();
+});
